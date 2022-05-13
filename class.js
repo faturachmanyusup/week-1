567206d2ae236896a98da7aa07efe1cf1e5ff9a2
faturachmanyusup <faturachmany@gmail.com>
2022-05-13 22:29:36 +0700

    first commit

---
+++ class.js
@@ -0,0 +1,26 @@
+/**
+ * 1. Buatlah class dengan nama Tiger, yang mana class Tiger memiliki:
+ *  attributes:
+ *    - name          = (mengambil input dari constructor)
+ *    - height        = (mengambil input dari constructor)
+ *    - weight        = (mengambil input dari constructor)
+ *    - energy        = 100
+ *    - isDangerous   = true
+ *  method:
+ *    - roar          = mengeluarkan output/return "Roarrrrr"
+ *    - die           = merubah isDangerous menjadi false
+ */
+
+
+/**
+ * 2. Buatlah class dengan nama Cat yang mana adalah child dari class Tiger (extent Tiger)
+ *  attributes:
+ *    - name          = (mengambil input dari constructor)
+ *    - height        = (mengambil input dari constructor)
+ *    - weight        = (mengambil input dari constructor)
+ *    - energy        = 10
+ *    - isDangerous   = false
+ *  method:
+ *    - roar          = mengeluarkan output/return "Roarrrrr"
+ *    - die           = merubah isDangerous menjadi false
+ */
---
+++ function.js
@@ -0,0 +1,11 @@
+/**
+ * 1. Buatlah sebuah function dengan nama countToTen
+ *  parameter
+ *    - start
+ *  output
+ *    - urutan angka string mulai dari start sampai dengan 10 (dipisahkan dengan spasi)
+ * 
+ * CONTOH:
+ *  input = 5
+ *  output = 5 6 7 8 9 10
+ */
